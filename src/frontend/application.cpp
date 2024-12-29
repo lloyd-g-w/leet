4f2c88781da74113876a9e0e96bef794dadc0021
lloyd-fedora <lloyd.williams1062@gmail.com>
2024-12-30 01:51:31 +1100

    🙃

--- src/frontend/application.cpp
+++ src/frontend/application.cpp
@@ -4,8 +4,8 @@
 
 app::app(std::string window_name, int window_width, int window_height,
          int grid_rows, int grid_cols) {
-    int rows = 500000000;
-    int cols = 500000000;
+    int rows = 5000;
+    int cols = 5000;
     p_window = new window(1920, 1080, "Holy Sheet");
     p_cell_grid = new cells_std::cell_grid(rows, cols);
     p_grid_gui = new grid_gui(*p_cell_grid, *p_window);
